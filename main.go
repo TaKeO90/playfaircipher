bacd9245acc3fda27f3b997591d72273cefe7f29
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2020-08-22 15:57:32 +0100

    introduce flags & go.mod file

--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
-	"./cipherlogic"
+	"flag"
 	"fmt"
+
+	"github.com/TaKeO90/playfaircipher/cipherlogic"
 )
 
-//TODO: set up flags for the program
+func flagParser(keyword, word *string, encrypt, decrypt *bool) {
+	flag.StringVar(keyword, "keyword", "", "specify the keyword that will generate a new matrix each time you change this keyword")
+	flag.StringVar(word, "word", "", "the word you want to encrpyt or decrypt")
+	flag.BoolVar(encrypt, "encrypt", false, "use this flag if you want to encrypt the word")
+	flag.BoolVar(decrypt, "decrypt", false, "use this flag if you want to decrypt the word")
+	flag.Parse()
+}
 
 func main() {
-	keyword := "monarchy"
-	pf := cipherlogic.NewMtx(keyword, "hello", true, false)
-	matrix := pf.GenMatrix()
-	fmt.Println(matrix)
-	wl := pf.EncOrDec()
-	fmt.Println(wl)
+	var (
+		encrypt bool
+		decrypt bool
+		keyword string
+		word    string
+	)
+	flagParser(&keyword, &word, &encrypt, &decrypt)
+
+	if keyword != "" && word != "" && encrypt && !decrypt {
+		pf := cipherlogic.NewMtx(keyword, word, encrypt, decrypt)
+		pf.GenMatrix()
+		result := pf.EncOrDec()
+		fmt.Println(result)
+	} else if keyword != "" && word != "" && decrypt && !encrypt {
+		pf := cipherlogic.NewMtx(keyword, word, encrypt, decrypt)
+		pf.GenMatrix()
+		result := pf.EncOrDec()
+		fmt.Println(result)
+	} else {
+		flag.PrintDefaults()
+	}
 }
